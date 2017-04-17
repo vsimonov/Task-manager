8b63560bb3314d9d5f093ea25a19c5c8d921e3d4
Vladimir <sir.vladimir-simonov@yandex.ru>
2017-04-18 02:06:52 +0300

    1

--- fb_yomamabot/views.py
+++
@@ -1,7 +0,0 @@
-# yomamabot/fb_yomamabot/views.py
-from django.views import generic
-from django.http.response import HttpResponse
-# Create your views here.
-class YoMamaBotView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World!")
